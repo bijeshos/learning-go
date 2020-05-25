20b428d7b0f131ff600ae83f6e1ad9caa020ac61
Bijesh O S <bijeshos@users.noreply.github.com>
2020-05-25 13:35:33 +0530

    added short variable declaration example

---
+++ level-01-fundamentals/topic-02-variables/example-04-short-variable-declaration.go
@@ -0,0 +1,17 @@
+/*
+A basic program to demonstrate short variable declaration
+
+Reference: https://tour.golang.org
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	var i, j int = 1, 2
+	k := 3
+	c, python, java := true, false, "no!"
+
+	fmt.Println(i, j, k, c, python, java)
+}
