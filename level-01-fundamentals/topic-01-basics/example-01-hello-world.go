f010ee022cf3f7ae459590c5f6e16ee9c8652fef
Bijesh O S <bijeshos@users.noreply.github.com>
2020-05-25 13:24:09 +0530

    modified hello-world example

---
+++ level-01-fundamentals/topic-01-basics/example-01-hello-world.go
@@ -0,0 +1,14 @@
+/*
+This is a simple hello world program
+
+Reference: https://tour.golang.org
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	//Println is part of fmt package
+	fmt.Println("Hello there!")
+}
