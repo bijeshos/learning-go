19054b69e4f90c2606ab7aa327bfc0783a59fac7
Bijesh O S <bijesh.os@gmail.com>
2020-05-27 21:52:20 +0530

    added example to show switch's evaluation order

---
+++ level-01-fundamentals/topic-03-flowcontrol/example-08-switch-evaluation-order.go
@@ -0,0 +1,27 @@
+/*
+A basic program to demonstrate evaluation order for 'switch'
+
+Reference: https://tour.golang.org
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("When's Saturday?")
+	today := time.Now().Weekday()
+	switch time.Saturday {
+	case today + 0:
+		fmt.Println("Today.")
+	case today + 1:
+		fmt.Println("Tomorrow.")
+	case today + 2:
+		fmt.Println("In two days.")
+	default:
+		fmt.Println("Too far away.")
+	}
+}
