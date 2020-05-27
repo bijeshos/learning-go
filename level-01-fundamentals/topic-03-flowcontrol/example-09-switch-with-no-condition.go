dbfedceb81de26829ff3468d97aa0b81c53303d4
Bijesh O S <bijesh.os@gmail.com>
2020-05-27 21:52:43 +0530

    added example for swith with no condition

---
+++ level-01-fundamentals/topic-03-flowcontrol/example-09-switch-with-no-condition.go
@@ -0,0 +1,24 @@
+/*
+A basic program to demonstrate usage of switch with no condition
+
+Reference: https://tour.golang.org
+*/
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	t := time.Now()
+	switch {
+	case t.Hour() < 12:
+		fmt.Println("Good morning!")
+	case t.Hour() < 17:
+		fmt.Println("Good afternoon.")
+	default:
+		fmt.Println("Good evening.")
+	}
+}
