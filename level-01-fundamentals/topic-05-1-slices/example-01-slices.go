854337d49a6dfce199f56deb282cdd2740dc739f
Bijesh O S <bijesh.os@gmail.com>
2020-05-30 22:11:38 +0530

    added initial version of slices example

---
+++ level-01-fundamentals/topic-05-1-slices/example-01-slices.go
@@ -0,0 +1,17 @@
+/*
+A simple program to demonstrate xxx
+
+Reference: https://tour.golang.org
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+
+	// index 1 to index 3 (excluding 4)
+	var s []int = primes[1:4]
+	fmt.Println(s)
+}
