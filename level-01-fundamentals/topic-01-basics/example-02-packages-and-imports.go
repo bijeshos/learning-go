ebaea34fb9d90177b35b99bf96832728bb47c851
Bijesh O S <bijesh.os@gmail.com>
2020-06-02 22:24:59 +0530

    added more examples

--- level-01-fundamentals/topic-01-basics/example-02-packages-and-imports.go
+++ level-01-fundamentals/topic-01-basics/example-02-packages-and-imports.go
@@ -13,11 +13,18 @@ import (
 	"fmt"
 	"math"
 	"math/rand"
+	"time"
 )
 
 func main() {
 
-	//math is one of the imported packages and Sqrt() is a function within math package
+	//fmt is an imported package and Println() is a function within fmt package
+	fmt.Println("This is a test message")
+
+	//time is another imported package and Now() is a function within time package. Now() does not have any arguments
+	fmt.Println("Current time is: ", time.Now())
+
+	//math is one of the imported packages and Sqrt() is a function within math package. Sqrt() expects an input argument
 	fmt.Printf("Square root of 7 is: %g \n", math.Sqrt(7))
 
 	//Pi is an exported value in math package
